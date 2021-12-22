00bc1913942cd900a6cc0bbcfdaf1b1bb5978057
Vishnu Nalagatla <vishnuvardhanreddy.n@techouts.com>
2021-12-22 13:03:39 +0530

    The GraphQL server for react native

---
+++ Server/DisplayData.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { useQuery, gql } from '@apollo/client';
+
+const QUERY_ALL_USERS = gql`
+    query GetAllUsers {
+        users {
+            id
+            
+            username
+            name
+            age
+            nationality
+        }
+    }
+`
+
+// const QUERY = gql`
+//         products(filter: { sku: { eq: "Flavourful Fusions - Kaju (500 gms)" } }) {
+//                 items {
+//                 name
+//                 sku
+//                 url_key
+//                 stock_status
+//                 price_range {
+//                 minimum_price {
+//                 regular_price {
+//                 value
+//                 currency
+//                 }
+//                 }
+//                 }
+//             }
+//                 total_count
+//                 page_info {
+//                 page_size
+//                 }
+//                 }
+// `
+
+const DisplayData = () => {
+    const { data: userData, loading: userLoading } = useQuery(QUERY_ALL_USERS);
+    if (userLoading) {
+        return <Text>Data is Loading......</Text>
+    }
+    if (userData) {
+        console.log(userData, "userData");
+    } else {
+        console.log('error');
+    }
+    return (
+        <>
+            <ScrollView>
+                <View>
+                    <Text>List of Users</Text>
+                </View>
+
+                {userData && userData.users.map((user) => {
+                    return (
+                        <View style={styles.viewContainer} key={user.id}>
+                            <Text style={styles.textContainer}>{user.name}</Text>
+                            <Text style={styles.textContainer}>{user.age}</Text>
+                            <Text style={styles.textContainer}>{user.nationality}</Text>
+                            <Text style={styles.textContainer}>{user.username}</Text>
+                        </View>
+                    )
+                }
+                )}
+            </ScrollView>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewContainer: {
+        margin: 20,
+    },
+    textContainer: {
+        margin: 5,
+    }
+});
+
+export default DisplayData
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+import { View, Text } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
+import DisplayData from './src/components/DisplayData';
+
+const App = () => {
+  // Initialize Apollo Client
+  const client = new ApolloClient({
+    uri: 'http://172.18.97.81:4000/graphql',  
+    cache: new InMemoryCache()
+  });
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <DisplayData></DisplayData>
+      </ApolloProvider>
+    </>
+  )
+}
+
+export default App
